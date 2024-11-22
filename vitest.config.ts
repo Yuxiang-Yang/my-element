e2624ce2e7d2c6c372ba2d440a6881241c63113a
Yuxiang Yang <yuxiangy@foxmail.com>
2024-11-22 23:46:16 +0800

    test: 配置Vitest

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+/// <reference types="vitest/config" />
+
+import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+})
